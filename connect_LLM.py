65179b18c9dd30351204fda3540373a495d4b1de
Darian-Lee-YTKA <136937505+Darian-Lee-YTKA@users.noreply.github.com>
2025-03-12 18:58:21 -0700

    putting my stashed changes here

--- connect_LLM.py
+++ connect_LLM.py
@@ -16,12 +16,12 @@ transformers.set_seed(seed)
 
 # Create prompt template for LLM
 prompt_tmplt = """
-Answer the question based on the following context, in a concise and summarized manner. Limit your response to 300 tokens or less and always finish your sentence! Make sure to format your answer so it is clear:
+Answer the question based on the following context, in a concise and summarized manner. Limit your response to 300 tokens or less and always finish your sentence! Make sure to format your answer so it is clear and DO NOT repeat the question:
 
 {context}
 
 ------
-Give a concise clear answer to the question based on the above context, limit your response to 300 tokens or less and make sure to summarize, rarely use lists: {question}
+"Give a concise clear answer to the question based on the above context, limit your response to 300 tokens or less and make sure to summarize. Do not repeat the users question: {question}
 """
 
 # context is the retrieved content from the database (db)
@@ -51,11 +51,11 @@ def query_rag(question):
 
     prompt = prompt_template.format(context=context_text, question=question)
 
-    output = generator(prompt, max_new_tokens=1000, top_k=10)
+    output = generator(prompt, max_new_tokens=300, top_k=10)
 
     generated = output[0]["generated_text"]
 
-    to_remove = "Give a concise clear answer to the question based on the above context, limit your response to 300 tokens or less and make sure to summarize, rarely use lists:"
+    to_remove = "Give a concise clear answer to the question based on the above context, limit your response to 300 tokens or less and make sure to summarize. Do not repeat the users question:"
 
     ques_answer = generated.split(to_remove)[1]
 
@@ -63,12 +63,12 @@ def query_rag(question):
 
     return answer
 
-parser = argparse.ArgumentParser()
+'''parser = argparse.ArgumentParser()
 parser.add_argument("question", type=str, help="The question for model.")
 args = parser.parse_args()
-output = query_rag(args.question)
+output = query_rag(args.question)'''
 
-print(output)
+
 
 
 
